9d6b7f69f6839fa06bfe8091a6582ace6ecdf3e2
Kristopher Hicks <kristopher.t.hicks@gmail.com>
2023-01-26 02:29:20 -0500

    Changes to be committed: 	modified:   index.html 	new file:   renderer.js
    Added a renderer script inside index.html

---
+++ renderer.js
@@ -0,0 +1,5 @@
+// This file is used to control the index.html
+
+document.getElementById('confirmButton').addEventListener("click", () => {
+    console.log(document.getElementById('nameInput').value);
+})
