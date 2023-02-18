15f7c31d696a57d213f6f93593b22e39b4fe5223
Kristopher Hicks <kristopher.t.hicks@gmail.com>
2023-02-18 13:03:27 -0500

    Basic outline of account prototype

--- accounts.js
+++ accounts.js
@@ -1,3 +1,11 @@
 const AccountPrototype = {
-    
+    name: 'undef',
+    timestamp: -1,
+    username: 'undef',
+    password: 'undef', // Only thing that needs encrypted
+    privileges: [],
+};
+
+const RestaurantAccountPrototype = {
+    role: 'undef', // The role will determine what you have access to
 };
